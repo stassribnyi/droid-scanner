50f491b4c78db974276483a7285c3281c43b0ca2
rserebrianskyi <r.serebrianskyi.ext@meteocontrol.com>
2023-10-08 12:14:32 +0300

    draft users

--- backend/src/main/kotlin/com/bright/hackaton/demo/controller/DroidsController.kt
+++ backend/src/main/kotlin/com/bright/hackaton/demo/controller/DroidsController.kt
@@ -1,14 +1,17 @@
 package com.bright.hackaton.demo.controller
 
 import com.bright.hackaton.demo.model.Droid
-import com.bright.hackaton.demo.service.DroidService
+import com.bright.hackaton.demo.model.User
+import com.bright.hackaton.demo.service.DroidsService
+import com.bright.hackaton.demo.service.UserService
 import kotlinx.coroutines.flow.Flow
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/droids")
-class DroidController(private val droidService: DroidService) {
+class DroidController(private val droidService: DroidsService, private val userService: UserService) {
+
     @PostMapping
     suspend fun createDroid(@RequestBody droid: Droid): ResponseEntity<Droid> {
         val createdDroid = droidService.createDroid(droid)
@@ -19,5 +22,11 @@ class DroidController(private val droidService: DroidService) {
     fun getAllDroids(): ResponseEntity<Flow<Droid>> {
         val droids = droidService.getAllDroids()
         return ResponseEntity.ok(droids)
+    }
+
+    @PostMapping
+    suspend fun createUser(): ResponseEntity<User> {
+        val createdUser = userService.createUser()
+        return ResponseEntity.ok(createdUser)
     }
 }
---
+++ backend/src/main/kotlin/com/bright/hackaton/demo/util/RandomNicknamePicker.kt
@@ -0,0 +1,67 @@
+package com.bright.hackaton.demo.util
+
+import org.springframework.stereotype.Service
+import java.util.*
+
+fun pickRandomNickNameAndModifyExisting(): String {
+    val names = listOf(
+        "Luke Skyjoker",
+        "Princess Leia Buns Solo",
+        "Han Brolo",
+        "Chewbacca the Hairy Hugger",
+        "Obi-Wan Cannoli",
+        "Darth Tater",
+        "Jar Jar Slinks",
+        "Yoda Soda",
+        "R2-Tea-2",
+        "C-3POpcorn",
+        "Jabba the Hutt-belly",
+        "Lando Calrispian",
+        "Admiral Snackbar",
+        "Boba Fettuccine",
+        "Emperor Palpa-Bean",
+        "Darth Vaper",
+        "Count Chocolate Dooku",
+        "Wookiee Cookies",
+        "Greedo the Green Bean",
+        "R2-Double-D2",
+        "Princess Leia Doughnut",
+        "Luke Frywalker",
+        "Chewbacca Chew-toy",
+        "Obi-Wan Kenolive",
+        "Han SoyLo",
+        "C-3P-Onion",
+        "Admiral Ackbarbecue",
+        "Ewok Eclair",
+        "Salacious Crumb Cake",
+        "Qui-Gon Jinn and Juice",
+        "Boba Feta Cheese",
+        "Wedge Antillettuce",
+        "Darth Salad Vader",
+        "Lando Calrissian Vinaigrette",
+        "Jabba the Hutt-dog",
+        "General Gravy-ous",
+        "R2-Tea-3PO",
+        "Padmé Amidala Pickles",
+        "Ewok Waffles",
+        "Chewbacca Chocolate Chip",
+        "Jar Jar Meringue",
+        "Darth Maulted Milk",
+        "Yoda Soda Float",
+        "Mon Mothma Muffin",
+        "Admiral Snack-pars",
+        "Porkins and Beans",
+        "Bossk-nack",
+        "C-3P-Broccoli",
+        "Porg-corn",
+        "Lobot-ster Roll"
+    )
+    val random = Random()
+    val randomIndex = random.nextInt(names.size)
+    val randomName = names[randomIndex]
+
+    // Append "random number" to the random name
+    val modifiedName = "$randomName 1"
+
+    return modifiedName
+}
