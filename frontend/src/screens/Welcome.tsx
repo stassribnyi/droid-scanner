ee0865eb1f9e2942a6cbd2078185b317f6d20bd5
Stas Sribnyi <stas.sribnyi@gmail.com>
2023-10-11 00:24:53 +0300

    Add nickname validation

--- frontend/src/screens/Welcome.tsx
+++ frontend/src/screens/Welcome.tsx
@@ -57,92 +57,99 @@ export const Welcome = () => {
 
   return (
     <ScreenContent>
-      <Grid
-        container
-        justifyContent='center'
-        spacing={3}
-        sx={{
-          paddingTop: '2rem',
-          paddingBottom: '2rem',
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          register();
         }}
       >
-        <Grid>
-          <Typography
-            variant='h5'
-            style={{
-              color: 'black',
-              fontWeight: 'bold',
-              textTransform: 'uppercase',
-              WebkitTextStroke: '1.5px #ff6855',
-              letterSpacing: '1px',
-            }}
-          >
-            Droids of Republic
-          </Typography>
-        </Grid>
-        <Grid xs={12}>
-          <Card
-            sx={{
-              background: 'none',
-            }}
-          >
-            <CardMedia
-              sx={{ height: 340 }}
-              image='/droid-scanner/welcome.png'
-              title='green iguana'
-            />
-            <CardContent
-              sx={{
-                marginTop: '-6rem',
-                borderRadius: '2rem',
-                background: 'rgba(35, 45, 60, 0.4)',
-                borderBottomLeftRadius: '1rem',
-                borderBottomRightRadius: '1rem',
-                backdropFilter: 'blur(8px)',
+        <Grid
+          container
+          justifyContent='center'
+          spacing={3}
+          sx={{
+            paddingTop: '2rem',
+            paddingBottom: '2rem',
+          }}
+        >
+          <Grid>
+            <Typography
+              variant='h5'
+              style={{
+                color: 'black',
+                fontWeight: 'bold',
+                textTransform: 'uppercase',
+                WebkitTextStroke: '1.5px #ff6855',
+                letterSpacing: '1px',
               }}
             >
-              <Stack direction='column' gap={2}>
-                <Typography
-                  gutterBottom
-                  variant='h5'
-                  component='div'
-                  align='center'
-                >
-                  Welcome to our ranks!
-                </Typography>
+              Droids of Republic
+            </Typography>
+          </Grid>
+          <Grid xs={12}>
+            <Card
+              sx={{
+                background: 'none',
+              }}
+            >
+              <CardMedia
+                sx={{ height: 340 }}
+                image='/droid-scanner/welcome.png'
+                title='green iguana'
+              />
+              <CardContent
+                sx={{
+                  marginTop: '-6rem',
+                  borderRadius: '2rem',
+                  background: 'rgba(35, 45, 60, 0.4)',
+                  borderBottomLeftRadius: '1rem',
+                  borderBottomRightRadius: '1rem',
+                  backdropFilter: 'blur(8px)',
+                }}
+              >
+                <Stack direction='column' gap={2}>
+                  <Typography
+                    gutterBottom
+                    variant='h5'
+                    component='div'
+                    align='center'
+                  >
+                    Welcome to our ranks!
+                  </Typography>
 
-                <TextField
-                  required
-                  label='What is your name, padawan?'
-                  variant='outlined'
-                  size='medium'
-                  value={nickname}
-                  onChange={({ target }) => setNickname(target.value)}
-                />
-                <Button
-                  disabled={generating}
-                  variant='text'
-                  color='primary'
-                  onClick={() => generateName()}
-                >
-                  Generate nickname
-                </Button>
-              </Stack>
-            </CardContent>
-          </Card>
+                  <TextField
+                    required
+                    label='What is your name, padawan?'
+                    variant='outlined'
+                    size='medium'
+                    value={nickname}
+                    onChange={({ target }) => setNickname(target.value)}
+                  />
+                  <Button
+                    disabled={generating}
+                    variant='text'
+                    color='primary'
+                    onClick={generateName}
+                  >
+                    Generate nickname
+                  </Button>
+                </Stack>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid xs={12}>
+            <Button
+              disabled={registering}
+              fullWidth
+              variant='contained'
+              size='large'
+              type='submit'
+            >
+              Join Now! (validate)
+            </Button>
+          </Grid>
         </Grid>
-        <Grid xs={12}>
-          <Button
-            disabled={registering}
-            fullWidth
-            variant='contained'
-            size='large'
-            onClick={register}
-          >
-            Join Now! (validate)
-          </Button>
-        </Grid>
-      </Grid>
+      </form>
     </ScreenContent>
   );
 };
