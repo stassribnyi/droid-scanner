092a5edd5d3ac123620e09dda538d5898a6661b7
Stas Sribnyi <stas.sribnyi@gmail.com>
2023-10-11 02:27:03 +0300

    Update security

--- frontend/src/hooks/useDeviceUUID.ts
+++ frontend/src/hooks/useDeviceUUID.ts
@@ -1,14 +1,14 @@
 import { useEffect, useState } from 'react';
 
 const DEVICE_UUID = 'DeviceUUID' as const;
-function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
-    .replace(/[xy]/g, function (c) {
-      const r = Math.random() * 16 | 0,
-        v = c == 'x' ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
-    });
-}
+// function uuidv4() {
+//   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+//     .replace(/[xy]/g, function (c) {
+//       const r = Math.random() * 16 | 0,
+//         v = c == 'x' ? r : (r & 0x3 | 0x8);
+//       return v.toString(16);
+//     });
+// }
 
 const getDeviceUUID = () => {
   const uuid = localStorage.getItem(DEVICE_UUID);
@@ -17,7 +17,7 @@ const getDeviceUUID = () => {
     return uuid;
   }
 
-  const randomUUID = uuidv4(); // crypto.randomUUID();
+  const randomUUID = crypto.randomUUID();
   localStorage.setItem(DEVICE_UUID, randomUUID);
 
   return randomUUID;
