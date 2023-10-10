f31f4f97c4f5046c92993c66ca621f3dc8b42124
vbahniuk <vbahniuk@brightgrove.com>
2023-10-10 10:54:58 +0300

    V.B. migration

--- backend/src/main/kotlin/com/bright/hackaton/demo/DemoDroidsApplication.kt
+++ backend/src/main/kotlin/com/bright/hackaton/demo/DemoDroidsApplication.kt
@@ -2,10 +2,8 @@ package com.bright.hackaton.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-
 class DemoDroidsApplication
 
 fun main(args: Array<String>) {
--- backend/src/main/kotlin/com/bright/hackaton/demo/config/migration/DroidsInitMigration.kt
+++ backend/src/main/kotlin/com/bright/hackaton/demo/config/migration/DroidsInitMigration.kt
@@ -1,4 +1,4 @@
-/*package com.bright.hackaton.demo.config.migration
+package com.bright.hackaton.demo.config.migration
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
@@ -9,7 +9,6 @@ import kotlinx.coroutines.reactive.asFlow
 import kotlinx.coroutines.reactive.awaitFirstOrNull
 import kotlinx.coroutines.runBlocking
 import org.bson.Document
-import org.bson.types.ObjectId
 import org.slf4j.LoggerFactory
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate
 import org.springframework.data.mongodb.core.query.Query
@@ -42,7 +41,7 @@ class DroidsInitMigration(private val reactiveMongoTemplate: ReactiveMongoTempla
     @Execution
     fun firstMigration(database: MongoDatabase) {
         runBlocking {
-            //createDroids()
+            createDroids()
         }
     }
 
@@ -61,17 +60,17 @@ class DroidsInitMigration(private val reactiveMongoTemplate: ReactiveMongoTempla
 
     suspend fun createDroids() {
         val droids =
-            jacksonObjectMapper().readValue<List<Droid>>(javaClass.getResource("/droids.json")!!)
-        val ids = mutableMapOf<String, ObjectId>()
+            jacksonObjectMapper().readValue<List<Droid>>(javaClass.getResource("/data/droids.json")!!)
+//        val ids = mutableMapOf<String, ObjectId>()
         val documents = droids.map { droid ->
             val document = Document()
-            document["_id"] = ids.computeIfAbsent(droid.id) { ObjectId() } //ToDo: take existing from json
+//            document["_id"] = ids.computeIfAbsent(droid.id) { ObjectId() } //ToDo: take existing from json
+            document["_id"] = droid.id
             document["name"] = droid.name
             document["description"] = droid.description
             document["number"] = droid.number
             document["isActivated"] = droid.isActivated
-            document
         }
         reactiveMongoTemplate.insert(documents, droidCollectionName).asFlow().collect()
     }
-}*/
+}
