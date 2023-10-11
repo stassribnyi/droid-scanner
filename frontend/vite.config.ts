3d38e51522646c30abca3d40d36b6522a8798bc4
Stas Sribnyi <stas.sribnyi@gmail.com>
2023-10-11 17:50:04 +0300

    Update deployment instructions

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,24 +4,28 @@ import react from '@vitejs/plugin-react'
 const FALLBACK_PORT = 3000
 const FALLBACK_PROXY_API = 'http://localhost:8080/'
 
+const config = {
+  host: true,
+  strictPort: true,
+  port: parseInt(process.env.PORT) || FALLBACK_PORT,
+  proxy: {
+    '/api': {
+      target: process.env.PROXY_API || FALLBACK_PROXY_API,
+      changeOrigin: true,
+      secure: false,
+      ws: true,
+    }
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '',
   server: {
-    watch: {
-      usePolling: true,
-    },
-    host: true,
-    strictPort: true,
-    port: parseInt(process.env.PORT) || FALLBACK_PORT,
-    proxy: {
-      '/api': {
-        target: process.env.PROXY_API || FALLBACK_PROXY_API,
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-      }
-    }
+    ...config
+  },
+  preview: {
+    ...config
   }
 })
