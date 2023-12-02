6c8f4059ab21c00e48226bb9592a5a8390443dc3
Stas Sribnyi <stas.sribnyi@gmail.com>
2023-12-02 16:57:29 +0200

    WIP: add list of quests, mark specific quest as current

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -50,6 +50,11 @@ const globalStyles: GlobalStylesProps['styles'] = {
     flexDirection: 'column',
     height: '100dvh',
   },
+
+  '.noise3': {
+    background: 'rgba(35, 45, 60, 0.9)',
+    backgroundImage: `url("data:image/svg+xml,%3C!-- svg: first layer --%3E%3Csvg viewBox='0 0 250 250' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='4' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`
+  }
 };
 
 export const Layout: FC<PropsWithChildren> = ({ children }) => (
--- frontend/src/screens/Quests.tsx
+++ frontend/src/screens/Quests.tsx
@@ -3,65 +3,171 @@ import {
   CardContent,
   Typography,
   Paper,
-  Collapse,
   Button,
   Box,
-  Unstable_Grid2 as Grid,
   Dialog,
   IconButton,
   CardMedia,
   Stack,
+  css,
 } from '@mui/material';
 import {
   Close,
-  ExpandLess,
-  ExpandMore,
-  QuestionMark,
+  Launch,
 } from '@mui/icons-material';
 import useAxios from 'axios-hooks';
-import { useEffect, useState } from 'react';
+import { FC, PropsWithChildren, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { useHintDroidId, useDeviceUUID, useAsyncAction } from '../hooks';
 import { BaseScreen } from '../components';
-import { collectedToRank } from '../utils';
 import { Droid, User } from '../types';
+import { useLocalStorage } from '@uidotdev/usehooks';
 
-const PLACEHOLDER_DROID: Droid = {
-  id: '',
-  name: 'Loading...',
-  description: `Loading...`,
-  order: 1,
-  activated: false,
-  hint: `Loading...`,
-  imageUrl: '',
-};
+const DRDialog: FC<PropsWithChildren<Readonly<{
+  open: boolean,
+  imageUrl: string,
+  onClose: () => void
+}>>> = ({ imageUrl, open, onClose, children }) => (
+  <Dialog open={open} onClose={onClose}>
+    <IconButton
+      edge='start'
+      color='inherit'
+      onClick={onClose}
+      aria-label='close'
+      sx={{
+        position: 'absolute',
+        top: '0.5rem',
+        right: '1rem',
+        zIndex: 1,
+      }}
+    >
+      <Close />
+    </IconButton>
+    <Card
+      sx={{
+        background: 'none',
+      }}
+    >
+      <Box sx={{ position: 'relative' }}>
+        <CardMedia
+          sx={{ height: 340 }}
+          image={imageUrl}
+          title='green iguana'
+        />
+        <Box
+          className='noise3'
+          sx={{
+            position: 'absolute',
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            content: "''",
+            opacity: 0.2
+          }}
+        />
+      </Box>
+
+      <CardContent
+        sx={{
+          marginTop: '-6rem',
+          borderRadius: '1.5rem',
+          background: 'rgba(35, 45, 60, 0.5)',
+          borderBottomLeftRadius: '1rem',
+          borderBottomRightRadius: '1rem',
+          backdropFilter: 'blur(16px)',
+        }}
+      >
+        {children}
+      </CardContent>
+    </Card>
+  </Dialog>
+)
+
+{/* TODO: add ripple */ }
+const QuestItem: FC<Readonly<{
+  description: string,
+  imageUrl: string,
+  variant: 'current' | 'active' | 'done',
+  onLaunch: () => void,
+}>> = ({ description, variant, imageUrl, onLaunch }) => (
+  <Stack
+    component={Paper}
+    direction='row'
+    alignItems='center'
+    justifyContent='space-between'
+    gap={1}
+    sx={{
+      border: variant === 'current' ? `2px solid #ff6855` : variant === 'active' ? "2px solid #999" : null,
+      borderRadius: '6px',
+      overflow: 'hidden',
+      padding: '0.25rem',
+      cursor: 'pointer'
+      // boxShadow: '1px 1px 6px #c4dec457',
+    }}
+    onClick={onLaunch}>
+    <Box
+      sx={{
+        width: 64,
+        height: 64,
+        minWidth: 64,
+
+      }}
+    >
+      <Box
+        component='img'
+        sx={{ height: '100%', width: '100%', mixBlendMode: 'lighten' }}
+        src={imageUrl}
+        alt="A quest giver image"
+      />
+    </Box>
+    <Typography
+      variant='caption'
+      sx={
+        css`
+        display: -webkit-box;
+        max-width: 400px;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        `
+      }>{description}</Typography>
+    <IconButton>
+      <Launch />
+    </IconButton>
+  </Stack>
+)
 
 export const Quests = () => {
   const { id } = useParams();
-  const [showHint, setShowHint] = useState(false);
+  const [current, setCurrent] = useState<Droid | null>(null);
   const [stored, setStored] = useHintDroidId();
   const deviceId = useDeviceUUID();
   const [showCongrats, setShowCongrats] = useState(false);
+  const [currentQuestId, setCurrentQuestId] = useLocalStorage<number | undefined>('current-quest-id')
 
-  const [{ data: found }, getDroid] = useAxios<Droid>(
+  const [{ data: droids }, getDroids] = useAxios<Droid[]>(
     {
-      url: `/api/droids/${deviceId}`,
+      url: `/api/droids`,
       params: {
         deviceId,
-        order: stored || 1,
       },
     },
     {
       manual: true,
     }
   );
-  const [{ data }, getUser] = useAxios<User>(`/api/users/${deviceId}`, {
+  const [{ data: user }, getUser] = useAxios<User>(`/api/users/${deviceId}`, {
     manual: true,
   });
 
+  console.log(currentQuestId);
+
+
   const [, getAll] = useAsyncAction(async () => {
-    await getDroid();
+    await getDroids();
     const { data } = await getUser();
 
     setShowCongrats(data.collectedDroids === data.totalDroids);
@@ -81,157 +187,81 @@ export const Quests = () => {
     getAll();
   }, [deviceId, stored]);
 
-  const droid = found ?? PLACEHOLDER_DROID;
-
-  const user = data || {
-    deviceId: deviceId,
-    name: 'loading...',
-    collectedDroids: 0,
-    totalDroids: 0,
-  };
-
   return (
-    <BaseScreen title='Current Quest'>
-      <Grid
-        container
-        direction='row'
-        justifyContent='space-between'
-        spacing={1}
-      >
-        <Grid xs={4}>
-          <Box
-            sx={{
-              height: '100px',
-              width: '100px',
-              border: '4px double #ff6855',
-              borderRadius: '10px',
-              overflow: 'hidden',
-              boxShadow: '1px 1px 6px #c4dec457',
-            }}
-          >
-            {droid.activated ? (
-              <Box
-                component='img'
-                sx={{ height: '100%', width: '100%' }}
-                src={`/droids/${droid.name.toLowerCase()}.jpg`}
-                alt={droid.name}
-              />
-            ) : (
-              <QuestionMark sx={{ height: '100%', width: '100%' }} />
-            )}
-          </Box>
-        </Grid>
-        <Grid xs={8}>
-          <Typography variant='body2' component='div'>
-            Greetings{' '}
-            <Typography variant='body2' component='span' color='#ff6855'>
-              {collectedToRank(user.collectedDroids, user.totalDroids)}
+    <BaseScreen title='Quests'>
+      <Box component='ul' sx={{ listStyle: 'none', p: 0, m: 0 }}>
+        {droids?.filter((d) => d.activated).map((d, idx) => <Box component='li' sx={{
+          mb: 2,
+        }}>
+          <>
+            {idx === 0 && <Typography variant='body2' color="primary" gutterBottom>Current</Typography>}
+            <QuestItem
+              variant={currentQuestId === d.order
+                ? 'current'
+                : !droids[idx + 1]?.activated
+                  ? 'active'
+                  : 'done'
+              }
+              description={d.hint}
+              imageUrl={`/droids/${d.name.toLowerCase()}.jpg`}
+              onLaunch={() => setCurrent(d)} />
+            {idx === 0 && <hr color='#333' style={{ marginTop: '1rem' }} />}
+          </>
+        </Box>)}
+      </Box>
+      <DRDialog imageUrl={current ? `/droids/${current.name.toLowerCase()}.jpg` : ''} open={!!current} onClose={() => setCurrent(null)}>
+        <Stack direction='column' gap={2}>
+          <Typography variant='body2' >
+            <Typography component='span' variant='inherit' color="primary" sx={{ mr: 1, fontWeight: 'bold' }}>
+              {current?.name}:
             </Typography>
-            !{' '}
-            {droid?.activated
-              ? `I have a small task for you. It will test your skills and
-            challenge your mind. Shell we begin?`
-              : `You don't know where this droid is, but he left some task for you...`}
+            <Typography
+              component='span'
+              variant='inherit'
+              sx={{ fontStyle: 'italic', }}
+              align='justify'
+            >
+              {current?.hint}
+              {current?.hint}
+              {current?.hint}
+            </Typography>
+
           </Typography>
-        </Grid>
-      </Grid>
-      <Card sx={{ mb: 2, mt: 2 }}>
-        <CardContent sx={{ background: 'rgba(35, 45, 60, 0.4)' }}>
-          <Typography
-            gutterBottom
-            variant='body2'
-            sx={{ textIndent: '2rem' }}
-            align='justify'
-          >
-            {droid.hint}
-          </Typography>
-        </CardContent>
-      </Card>
-      <Paper sx={{ p: 2, width: '100%' }}>
-        {droid.activated ? (
-          <Button onClick={() => setShowHint((value) => !value)}>
-            Who am I?
-            {showHint ? <ExpandLess /> : <ExpandMore />}
+          <Button sx={{ justifySelf: 'center' }} onClick={() => {
+            console.log(current);
+
+            setCurrentQuestId(current?.order)
+
+            setCurrent(null)
+          }}>
+            Accept
           </Button>
-        ) : (
-          <Box sx={{ p: 1 }}>
-            <Typography color='info' variant='caption'>
-              Find this droid to learn more about him.
-            </Typography>
-          </Box>
-        )}
-        <Collapse in={showHint} timeout='auto' unmountOnExit>
+        </Stack>
+      </DRDialog>
+      <DRDialog imageUrl="/welcome.png" open={showCongrats} onClose={() => setShowCongrats(false)}>
+        <Stack direction='column' gap={2}>
+          <Typography align='center' color='#ff6855' variant='h6'>
+            Congratulations!
+          </Typography>
           <Typography
             gutterBottom
-            variant='body2'
-            sx={{ textIndent: '2rem' }}
+            variant='body1'
+            component='div'
             align='justify'
+            sx={{}}
           >
-            {droid.description}
+            A fantastic achievement! You've finished all the quests we've
+            prepared for you. You have proven yourself to be a master jedi.
           </Typography>
-        </Collapse>
-      </Paper>
-      <Dialog open={showCongrats} onClose={() => setShowCongrats(false)}>
-        <IconButton
-          edge='start'
-          color='inherit'
-          onClick={() => setShowCongrats(false)}
-          aria-label='close'
-          sx={{
-            position: 'absolute',
-            top: '0.5rem',
-            right: '1rem',
-            zIndex: 1,
-          }}
-        >
-          <Close />
-        </IconButton>
-        {/* Consider reusing? */}
-        <Card
-          sx={{
-            background: 'none',
-          }}
-        >
-          <CardMedia
-            sx={{ height: 340 }}
-            image='/welcome.png'
-            title='green iguana'
-          />
-          <CardContent
-            sx={{
-              marginTop: '-6rem',
-              borderRadius: '1.5rem',
-              background: 'rgba(35, 45, 60, 0.5)',
-              borderBottomLeftRadius: '1rem',
-              borderBottomRightRadius: '1rem',
-              backdropFilter: 'blur(16px)',
-            }}
-          >
-            <Stack direction='column' gap={2}>
-              <Typography align='center' color='#ff6855' variant='h6'>
-                Congratulations!
-              </Typography>
-              <Typography
-                gutterBottom
-                variant='body1'
-                component='div'
-                align='justify'
-                sx={{}}
-              >
-                A fantastic achievement! You've finished all the quests we've
-                prepared for you. You have proven yourself to be a master jedi.
-              </Typography>
-              <Typography align='center' sx={{ fontWeight: '700' }}>
-                May the force be with you!
-              </Typography>
-              <Typography variant='caption' align='center'>
-                Thanks for playing our little game, your Coders of Republic
-                team!
-              </Typography>
-            </Stack>
-          </CardContent>
-        </Card>
-      </Dialog>
+          <Typography align='center' sx={{ fontWeight: '700' }}>
+            May the force be with you!
+          </Typography>
+          <Typography variant='caption' align='center'>
+            Thanks for playing our little game, your Coders of Republic
+            team!
+          </Typography>
+        </Stack>
+      </DRDialog>
     </BaseScreen>
   );
 };
